1c893817754b0d55bbe2df2c8f68377b97da42b5
yangruijing <yrjword@163.com>
2018-10-31 16:20:48 +0800

    test commit

---
+++ index.py
@@ -0,0 +1,12 @@
+#!/user/bin/env pythonb
+# -*- coding: utf-8 -*-
+'''
+@author: v_yangruijing
+@email: v_yangruijing@baidu.com
+@file: index.py
+@time: 18-10-31 下午3:54
+@desc:
+'''
+
+if __name__=="__main__":
+    print '测试一下哈哈哈哈哈'
